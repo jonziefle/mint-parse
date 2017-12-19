e4dc4b6fae4699c70670c65cce5c45597458b684
Jon Ziefle <jziefle@metlife.com>
2017-12-19 13:16:59 -0500

    added more processing features for the output

--- mint-parse.py
+++ mint-parse.py
@@ -2,14 +2,40 @@ import os
 import sys
 import csv
 import argparse
+from decimal import Decimal
 
 # global variables
-categoryObject = {}
-dateArray = ["1/2017","2/2017","3/2017","4/2017","5/2017","6/2017","7/2017","8/2017","9/2017","10/2017","11/2017","12/2017"]
+categories = {
+    "Auto & Transport" : {
+        "sumTotal": [],
+        "subcategories": {
+            "Auto & Transport" : [],
+            "Auto Insurance" : [],
+            "Auto Insurance" : [],
+            "Auto Payment" : [],
+            "Gas & Fuel" : [],
+            "Parking" : [],
+            "Public Transportation" : [],
+            "Service & Parts" : [],
+        }
+    },
+    "Misc Expenses" : {
+        "sumTotal": [],
+        "subcategories": {}
+    }
+}
+
+dateList = ["1/2017","2/2017","3/2017","4/2017","5/2017","6/2017","7/2017","8/2017","9/2017","10/2017","11/2017","12/2017"]
 
 def main(inputFile, outputFile):
     # data object
     data = {}
+
+    # initialize categories
+    for category in categories:
+        categories[category]["sumTotal"] = [0] * 12
+        for subcategory in categories[category]["subcategories"]:
+            categories[category]["subcategories"][subcategory] = [0] * 12
 
     # open and process csv file
     with open(inputFile, newline='') as csvfile:
@@ -21,31 +47,49 @@ def main(inputFile, outputFile):
 
         # read csv rows
         for row in csvreader:
-            date = row[0].split("/")[0] + "/" + row[0].split("/")[2]
-            amount = row[3]
-            category = row[5]
+            csvDate = row[0].split("/")[0] + "/" + row[0].split("/")[2]
+            csvSubcategory = row[5]
 
-            if category not in categoryObject:
-                categoryObject[category] = [0] * 12
+            csvAmount = round(Decimal(row[3]), 2)
+            if (row[4] == "credit"):
+                csvAmount *= -1
 
-            if date in dateArray:
-                index = dateArray.index(date)
-                categoryObject[category][index] += float(amount)
+            if csvDate in dateList:
+                index = dateList.index(csvDate)
 
-    # sort arrays
-    dateArray.sort()
+                # set default category
+                csvCategory = ""
+                for category in categories:
+                    if csvSubcategory in categories[category]["subcategories"]:
+                        csvCategory = category
+                        break
 
-    # print csv data
+                # check to see if this is a new category
+                if csvCategory == "":
+                    csvCategory = "Misc Expenses"
+                    categories[csvCategory]["subcategories"][csvSubcategory] = [0] * 12
+
+                # add amount
+                categories[csvCategory]["sumTotal"][index] += csvAmount
+                categories[csvCategory]["subcategories"][csvSubcategory][index] += csvAmount
 
     # write csv data
     with open(outputFile, 'w', newline='') as csvfile:
         print("Writing: " + outputFile)
         csvwriter = csv.writer(csvfile)
 
-        csvwriter.writerow(["Category"] + dateArray)
+        # write categories
+        csvwriter.writerow(["Category"] + dateList)
+        for category in categories:
+            csvwriter.writerow([category] + categories[category]["sumTotal"])
 
-        for key, value in categoryObject.items():
-            csvwriter.writerow([key] + value)
+        csvwriter.writerow([])
+
+        # write subcategories
+        csvwriter.writerow(["Subcategory"] + dateList)
+        for category in categories:
+            for subcategory in categories[category]["subcategories"]:
+                csvwriter.writerow([subcategory] + categories[category]["subcategories"][subcategory])
 
 if __name__ == "__main__":
     # parses command line for input file and output path
